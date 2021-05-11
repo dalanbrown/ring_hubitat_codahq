ae17ed0386fba169c72ef2350935ea6041f7d949
Caleb Morse <morse.caleb@gmail.com>
2021-05-11 13:51:29 -0600

    Add panic button driver

---
+++ src/drivers/ring-virtual-panic-button.groovy
@@ -0,0 +1,100 @@
+/**
+ *  Ring Virtual Panic Button Driver
+ *
+ *  Copyright 2019 Ben Rimmasch
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ *  Change Log:
+ *  2020-08-16: Initial
+ */
+
+metadata {
+  definition(name: "Ring Virtual Panic Button", namespace: "ring-hubitat-codahq", author: "Ben Rimmasch",
+    importUrl: "https://raw.githubusercontent.com/codahq/ring_hubitat_codahq/master/src/drivers/ring-virtual-panic-button.groovy") {
+    capability "Refresh"
+    capability "Battery"
+    capability "TamperAlert"
+
+    attribute "lastCheckin", "string"
+  }
+
+  preferences {
+    input name: "descriptionTextEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: false
+    input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: false
+    input name: "traceLogEnable", type: "bool", title: "Enable trace logging", defaultValue: false
+  }
+}
+
+private logInfo(msg) {
+  if (descriptionTextEnable) log.info msg
+}
+
+def logDebug(msg) {
+  if (logEnable) log.debug msg
+}
+
+def logTrace(msg) {
+  if (traceLogEnable) log.trace msg
+}
+
+def refresh() {
+  logDebug "Attempting to refresh."
+  //parent.simpleRequest("refresh-device", [dni: device.deviceNetworkId])
+}
+
+def setValues(deviceInfo) {
+  logDebug "updateDevice(deviceInfo)"
+  logTrace "deviceInfo: ${deviceInfo}"
+
+  if (deviceInfo.state && deviceInfo.state.testMode != null) {
+    checkChanged("testMode", deviceInfo.state.testMode)
+  }
+  if (deviceInfo.batteryLevel != null) {
+    checkChanged("battery", deviceInfo.batteryLevel, "%")
+  }
+  if (deviceInfo.tamperStatus) {
+    checkChanged("tamper", deviceInfo.tamperStatus == "tamper" ? "detected" : "clear")
+  }
+  
+  for(key in ['impulseType', 'lastCommTime', 'lastUpdate', 'nextExpectedWakeup', 'signalStrength']) {
+    if (deviceInfo[key]) {
+      state[key] = deviceInfo[key]
+    }
+  }
+  
+  if (deviceInfo?.impulseType == "comm.heartbeat") {
+    sendEvent(name: "lastCheckin", value: convertToLocalTimeString(new Date()), displayed: false, isStateChange: true)
+  }
+  
+  for(key in ['firmware', 'hardwareVersion']) {
+    if (deviceInfo[key] && device.getDataValue(key) != deviceInfo[key]) {
+      device.updateDataValue(key, deviceInfo[key])
+    }
+  }
+}
+
+def checkChanged(attribute, newStatus, unit=null) {
+  if (device.currentValue(attribute) != newStatus) {
+    logInfo "${attribute.capitalize()} for device ${device.label} is ${newStatus}"
+    sendEvent(name: attribute, value: newStatus, unit: unit)
+  }
+}
+
+private convertToLocalTimeString(dt) {
+  def timeZoneId = location?.timeZone?.ID
+  if (timeZoneId) {
+    return dt.format("yyyy-MM-dd h:mm:ss a", TimeZone.getTimeZone(timeZoneId))
+  }
+  else {
+    return "$dt"
+  }
+}
